ac45a0bb7aed97f57d6754741967f0cbcf434580
sklalaludek <pattiegolis@gmail.com>
2018-02-05 22:54:57 +0100

    zadanie dnia 1

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,9 @@
 //Twój kod
+const numb = process.argv.slice(2);
+const arrOfNumb = numb.map(Number).sort();
+
+arrOfNumb.forEach((liczba) => {
+    setTimeout(() => {
+        console.log(liczba);
+    }, 1000*liczba);
+});
